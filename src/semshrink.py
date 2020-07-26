ceed4337cbccdf60d7f93cff37ec7f8cff91f9f7
Rahul Gopinath <vrthra@users.noreply.github.com>
2020-07-26 23:29:12 +0200

    Update semshrink.py

--- src/semshrink.py
+++ src/semshrink.py
@@ -1,41 +1,79 @@
-import random
-import string
-import sys
-random.seed(int(sys.argv[1]))
-
-class SimpleFuzzer:
-    def __init__(self, grammar):
-        self.grammar = grammar
-
-
+class LimitFuzzer:
     def tree_to_str(self, tree):
         name, children = tree
         if not children: return name
         return ''.join(self.tree_to_str(c) for c in children)
 
-
     def select(self, lst):
         return random.choice(lst)
 
-    def unify_key(self, key):
-        if not key in self.grammar:
-            return (key, [])
-        return (key, self.unify_rule(self.select(self.grammar[key])))
+    def symbol_cost(self, grammar, symbol, seen):
+        if symbol in self.key_cost: return self.key_cost[symbol]
+        if symbol in seen:
+            self.key_cost[symbol] = float('inf')
+            return float('inf')
+        v = min((self.expansion_cost(grammar, rule, seen | {symbol})
+                    for rule in grammar.get(symbol, [])), default=0)
+        self.key_cost[symbol] = v
+        return v
 
-    def unify_rule(self, rule):
-        return [self.unify_key(token) for token in rule]
+    def expansion_cost(self, grammar, tokens, seen):
+        return max((self.symbol_cost(grammar, token, seen)
+                    for token in tokens if token in grammar), default=0) + 1
 
-    def fuzz(self, key):
-        v = self.unify_key(key)
-        return self.tree_to_str(v)
+    def gen_key(self, key, depth, max_depth):
+        if key not in self.grammar: return (key, [])
+        if depth > max_depth:
+            clst = sorted([(self.cost[key][str(rule)], rule) for rule in self.grammar[key]])
+            rules = [r for c,r in clst if c == clst[0][0]]
+        else:
+            rules = self.grammar[key]
+        return (key, self.gen_rule(self.select(rules), depth+1, max_depth))
 
-class ComplexFuzzer(SimpleFuzzer):
+    def gen_rule(self, rule, depth, max_depth):
+        return [self.gen_key(token, depth, max_depth) for token in rule]
+
+    def fuzz(self, key='<start>', max_depth=10):
+        return self.tree_to_str(self.gen_key(key=key, depth=0, max_depth=max_depth))
+
     def __init__(self, grammar):
+        self.grammar = grammar
+        self.key_cost = {}
+        self.cost = self.compute_cost(grammar)
+
+    def compute_cost(self, grammar):
+        cost = {}
+        for k in grammar:
+            cost[k] = {}
+            for rule in grammar[k]:
+                cost[k][str(rule)] = self.expansion_cost(grammar, rule, set())
+        return cost
+
+
+class ComplexFuzzer(LimitFuzzer):
+    def __init__(self, grammar):
+        def cfg(g):
+            return {k: [self.cfg_rule(r) for r in g[k]] for k in g}
+        super().__init__(cfg(grammar))
+        self.cfg_grammar = self.grammar
         self.grammar = grammar
         self.vars = []
         self._vars = []
 
-    def unify_rule(self, rule):
+    def cfg_rule(self, rule):
+        return [t[0] if isinstance(t, tuple) else t for t in rule]
+
+    def gen_key(self, key, depth, max_depth):
+        if key not in self.grammar: return (key, [])
+        if depth > max_depth:
+            clst_ = [(self.cost[key][str(self.cfg_rule(rule))], rule) for rule in self.grammar[key]]
+            clst = sorted(clst_, key=lambda x: x[0])
+            rules = [r for c,r in clst if c == clst[0][0]]
+        else:
+            rules = self.grammar[key]
+        return (key, self.gen_rule(self.select(rules), depth+1, max_depth))
+
+    def gen_rule(self, rule, depth, max_depth):
         ret = []
         for token_ in rule:
             if isinstance(token_, tuple):
@@ -44,9 +82,10 @@ class ComplexFuzzer(SimpleFuzzer):
             else:
                 token = token_
                 fns = {}
+
             pre = fns.get('pre', lambda s, t, x: x())
             post = fns.get('post', lambda s, x: x)
-            val = pre(self, token, lambda: self.unify_key(token))
+            val = pre(self, token, lambda: self.gen_key(token, depth, max_depth))
             v = post(self, val)
             ret.append(v)
         return ret
@@ -71,27 +110,11 @@ def sync(o, val):
 
 class ChoiceFuzzer(ComplexFuzzer):
     def __init__(self, grammar, choices):
-        self.grammar = grammar
-        self.vars = []
-        self._vars = []
+        super().__init__(grammar)
         self.choices = choices
 
     def select(self, lst):
         return self.choices.choice(lst)
-
-grammar = {
-        '<start>' : [[ '<assignments>' ]],
-        '<assignments>': [['<assign>', (';\n', {'post':sync})], ['<assign>', (';\n', {'post':sync}), '<assignments>']],
-        '<assign>': [[('<var>', {'post':defining_var}), ' = ', '<expr>']],
-        '<expr>': [
-            ['<expr>', ' + ', '<expr>'],
-            ['<expr>', ' - ', '<expr>'],
-            ['(', '<expr>', ')'],
-            [('<var>', {'pre':defined_var})],
-            ['<digit>']],
-        '<digit>': [['0'], ['1']],
-        '<var>': [[i] for i in string.ascii_lowercase]
-}
 
 
 class ChoiceSeq:
@@ -116,36 +139,6 @@ class ChoiceSeq:
     def choice(self, lst):
         return lst[self.i() % len(lst)]
 
-grammar = {
-        '<start>' : [[ '<assignments>' ]],
-        '<assignments>': [['<assign>', (';\n', {'post':sync})], ['<assign>', (';\n', {'post':sync}), '<assignments>']],
-        '<assign>': [[('<var>', {'post':defining_var}), ' = ', '<expr>']],
-        '<expr>': [
-            ['<expr1>', ' + ', '<expr1>'],
-            ['<expr1>', ' - ', '<expr1>'],
-            ['(', '<expr1>', ')'],
-            [('<var>', {'pre':defined_var})],
-            ['<digit>']],
-        '<digit>': [['0'], ['1']],
-        '<var>': [[i] for i in string.ascii_lowercase],
-
-        '<expr1>': [
-            ['<expr2>', ' + ', '<expr2>'],
-            ['<expr2>', ' - ', '<expr2>'],
-            ['(', '<expr2>', ')'],
-            [('<var>', {'pre':defined_var})],
-            ['<digit>']],
-
-        '<expr2>': [
-            [('<var>', {'pre':defined_var})],
-            ['<digit>']],
-}
-
-def pred(v):
-    if '((' in v and '))' in v:
-        return True
-    return False
-
 def remove_check_each_fragment(instr, start, part_len, causal):
     for i in range(start, len(instr), part_len):
         stitched =  instr[:i] + instr[i+part_len:]
@@ -162,48 +155,59 @@ def ddmin(cur_str, causal_fn):
             start, part_len = 0, part_len // 2
     return cur_str
 
+def pred(v):
+    if v is None: return False
 
-choices = ChoiceSeq()
+    if '((' in v and '))' in v:
+        return True
+    return False
 
-c1 = ChoiceFuzzer(grammar, choices)
-v1 = c1.fuzz('<start>')
-#print(pred(v1), "\n", v1)
-#print(c1.vars)
-#print(c1.choices.ints)
-
-choices = ChoiceSeq(c1.choices.ints)
-c2 = ChoiceFuzzer(grammar, choices)
-v2 = c2.fuzz('<start>')
-#print(pred(v2), "\n", v2)
-#print(c2.vars)
-#print(c2.choices.ints)
-
-# print(repr(v1), repr(v2))
-assert v1 == v2
-print(c2.choices.ints)
-
-
-def causal(ints):
+def ints_to_string(grammar, ints):
     choices = ChoiceSeq(ints)
     cf = ChoiceFuzzer(grammar, choices)
     try:
-        # print(ints)
-        v = cf.fuzz('<start>')
-        p = pred(v)
-        #print(p)
-        return p
+        return cf.fuzz('<start>')
     except IndexError:
-        # print('no')
-        return False
+        return None
 
-if pred(v2):
-    newv = ddmin(c1.choices.ints, causal)
-    choices = ChoiceSeq(newv)
-    cf = ChoiceFuzzer(grammar, choices)
-    # print(c1.choices.ints)
-    print('original:', v1, len(c1.choices.ints))
-    print('minimal:', cf.fuzz('<start>'), len(cf.choices.ints))
-    print(cf.choices.ints)
+if __name__ == '__main__':
+    import random
+    import string
+    import sys
+    random.seed(int(sys.argv[1]))
+    assignment_grammar = {
+            '<start>' : [[ '<assignments>' ]],
+            '<assignments>': [['<assign>', (';\n', {'post':sync})], ['<assign>', (';\n', {'post':sync}), '<assignments>']],
+            '<assign>': [[('<var>', {'post':defining_var}), ' = ', '<expr>']],
+            '<expr>': [
+                ['<expr>', ' + ', '<expr>'],
+                ['<expr>', ' - ', '<expr>'],
+                ['(', '<expr>', ')'],
+                [('<var>', {'pre':defined_var})],
+                ['<digit>']],
+            '<digit>': [['0'], ['1']],
+            '<var>': [[i] for i in string.ascii_lowercase]
+    }
+    choices = ChoiceSeq()
 
-# minimize: 2, 9, 18, 22, 54, 82, 100
-# no minimal: 49, 81
+    c = ChoiceFuzzer(assignment_grammar, choices)
+    val = c.fuzz('<start>')
+
+    causal_fn = lambda ints: pred(ints_to_string(assignment_grammar, ints))
+
+    if pred(val):
+        newv = ddmin(c.choices.ints, causal_fn)
+        choices = ChoiceSeq(newv)
+        cf = ChoiceFuzzer(assignment_grammar, choices)
+        print('original:', val, len(c.choices.ints))
+
+        while True:
+            newv = ddmin(cf.choices.ints, causal_fn)
+            if len(newv) >= len(cf.choices.ints):
+                break
+            cf = ChoiceFuzzer(assignment_grammar, ChoiceSeq(newv))
+
+        cf = ChoiceFuzzer(assignment_grammar, ChoiceSeq(newv))
+        print('minimal:', cf.fuzz('<start>'), len(newv))
+        print(cf.choices.ints)
+
